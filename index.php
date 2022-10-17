6709ec57ec0414aa496eca14eed53f5026387ec4
Satyam Sundaram <sundaramsatyam12@gmail.com>
2022-10-18 01:59:06 +0530

    Updated index

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
     <main class="form-signin">
         <form>
             <img class="mb-4" src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-            <h1 class="h3 mb-3 fw-normal">Login to Account</h1>
+            <h1 class="h3 mb-3 fw-normal">Sign-in here</h1>
 
             <div class="form-floating">
                 <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
