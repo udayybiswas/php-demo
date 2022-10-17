e6293ca892d19799f8e496dd177c05b56ae4febe
Satyam Sundaram <sundaramsatyam12@gmail.com>
2022-10-18 02:12:12 +0530

    updated placeholder

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
             <h1 class="h3 mb-3 fw-normal">Account Login</h1>
 
             <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <input type="email" class="form-control" id="floatingInput" placeholder="Your Email">
                 <label for="floatingInput">Email address</label>
             </div>
             <div class="form-floating">
